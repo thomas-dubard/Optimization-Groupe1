df70efa466b393d0cc8d3f8fb5a7efa3dc40d024
thomas-dubard <55085630+thomas-dubard@users.noreply.github.com>
2020-05-10 15:00:34 +0200

    ça marche :heart: :astonished:

--- draft.py
+++ draft.py
@@ -1,13 +1,16 @@
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
+import random
+import math
 """
 On se place directement entre t_0 et t_f.
 Alors N correspond à l'échantillonage entre les deux.
 """
-N = 100
+N = 20
 U = 230.0
-c = np.array([1.0 for _ in range(N)]) # coût constant
-Qf = 500.0
+#c = np.array([1.0 for _ in range(N)]) # coût constant
+c = np.array([2.0*(math.cos(i) + 1.1) for i in range(N)]) # coût oscillant
+Qf = 15.0
 Qi = 0.0
 t_f = 10.0
 t_i = 0.0
@@ -39,7 +42,7 @@ def test_min(lamb):
             return False
     return True
 
-def contraintesactivesOQP(xk=P_0, lambdak=[0]*(2*N + 1), W=W_0):
+def contraintesactivesOQP(xk=P_0, lambdak=np.array([0]*(2*N + 1)), W=W_0):
     """
     On implémente l'algorithme des contraintes actives QP.
     On est ici avec G=0, f(x)=x*c, c(x)=Ax-b
@@ -51,9 +54,13 @@ def contraintesactivesOQP(xk=P_0, lambdak=[0]*(2*N + 1), W=W_0):
     Elle est telle que A*p=0, ce qui est garanti par notre choix simple.
     Elle minimise c*p.
     """
-    while not test_min(lambdak):
+    compteur = 0
+    while not test_min(lambdak) and compteur<20:
+        compteur += 1
+        print(f"itération {compteur}")
         xswap = xk
         # (a) direction pk
+        print("a")
         pk = [0]*N
         indice = []
         for i in W:
@@ -64,13 +71,14 @@ def contraintesactivesOQP(xk=P_0, lambdak=[0]*(2*N + 1), W=W_0):
         indice = set(indice)
         if len(indice) < N - 1:
             liste = [x for x in range(N) if not x in indice]
-            i1 = random.randint(len(liste))
+            i1 = random.randint(0, len(liste)-1)
             y1 = liste.pop(i1)
-            i2 = random.randint(len(liste))
+            i2 = random.randint(0, len(liste)-1)
             y2 = liste.pop(i2)
             pk[y1], pk[y2] = c[y2], -c[y1]
             # (b) pk != 0
             # légitime car coût non nul à tout instant
+            print(f"b : pk={pk}, W={W}")
             swap = [1] + [None]*(2*N + 1)
             if not 0 in W and pk[y1] + pk[y2] < 0:
                 z = (Qi - Qf + (t_f - t_i)*sum(y for y in xk)/(N * U))
@@ -78,37 +86,56 @@ def contraintesactivesOQP(xk=P_0, lambdak=[0]*(2*N + 1), W=W_0):
             for i in range(N+1):
                 if not i in W and pk[i-1] < 0:
                     swap[i+1] = (-xk[i-1]/pk[i-1])
-                if not (i+N) in W and pk[i+N-1] > 0:
+                if not (i+N) in W and pk[i-1] > 0:
                     swap[i+1] = (xk[i-1]/pk[i-1])
             alphak = min(x for x in swap if x != None)
-            xk = xk + alphak * pk
+            xk = np.array([xk[i] + alphak * pk[i] for i in range(N)])
             if alphak < 1:
                 j = 0
                 while swap[j] != alphak:
                     j += 1
                 W.append(j)
+            print(f"xk={xk}, W={W}")
         # (c) pk = 0
         def grad_lag(xk, lambdak):
-            return grad_fun(xk) + np.dot(A.T, lambdak)
+            lambdak_full = np.array([0 if x==None else x for x in lambdak])
+            return grad_fun(xk) + np.dot(A.T, lambdak_full)
         def step_c(xk, lk, grad_f, grad_l, c, alpha=1e-2, maxit=1e3, eps=1e-8):
             i = 0
             grad_l_xk = grad_l(xk, lk)
-            while (i < maxit) and (grad_l_xk > eps):
+            while (i < maxit) and (np.linalg.norm(grad_l_xk > eps)):
+                i += 1
                 grad_l_xk = grad_l(xk, lk)
                 xk = xk - alpha*grad_l_xk
-                for x in lk:
-                    x = min(O, x + alpha*c(xk))
+                c_xk = c(xk)
+                for j in range(len(lk)):
+                    if j in W:
+                        lk[j] = min(0, lk[j] + alpha*c_xk[j])
             return xk, lk
-        if pk == [O]*N:
+        if pk == [0]*N:
+            print(f"c : pk={pk}")
             xk, lambdak = step_c(xk, lambdak, grad_fun, grad_lag, cont)
+            lambdaswap = []
             for i in range(2*N + 1):
                 if not i in W:
-                    lambdak[i] = None
-            swap = 0, lambdak[0]
+                    lambdaswap.append(None)
+                else:
+                    lambdaswap.append(lambdak[i])
+            lambdak = np.array(lambdaswap)
+            def swap_choose(lamdbak):
+                i = 0
+                while i < len(lambdak) and lambdak[i] == None:
+                    i += 1
+                return i, lambdak[i]
+            swap = swap_choose(lambdak)
             for i in range(len(lambdak)):
-                if lambdak[i] < swap[1]:
+                if lambdak[i] != None and lambdak[i] < swap[1]:
                     swap = i, lambdak[i]
             if swap[1] <= 0:
                 xk = xswap
                 W = [x for x in W if x != swap[0]]
+            print(f"xk={xk}, W={W}")
+    plt.plot(list(range(N)), xk, color='red')
+    plt.plot(list(range(N)), c, color='blue')
+    plt.show()
     return xk
